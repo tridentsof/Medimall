1af12df68506c03ac37e06fe4ecbb37ec24c1399
tridentsof <gahanhthao@gmail.com>
2022-03-18 18:20:25 +0700

    Admin Product v1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Medimall.Models;
+
+namespace Medimall.Controllers
+{
+    public class HomeController : Controller
+    {
+        MedimallEntities db = new MedimallEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Models/sysdiagram.cs
+++ Models/sysdiagram.cs
@@ -12,13 +12,12 @@ namespace Medimall.Models
     using System;
     using System.Collections.Generic;
     
-    public partial class ListDrug
+    public partial class sysdiagram
     {
-        public int ListId { get; set; }
-        public Nullable<int> ProductId { get; set; }
-        public Nullable<int> AccountId { get; set; }
-    
-        public virtual Account Account { get; set; }
-        public virtual Product Product { get; set; }
+        public string name { get; set; }
+        public int principal_id { get; set; }
+        public int diagram_id { get; set; }
+        public Nullable<int> version { get; set; }
+        public byte[] definition { get; set; }
     }
 }
